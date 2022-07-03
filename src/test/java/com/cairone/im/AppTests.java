af7a077d507822afb45ade637d8d0bafdaaf839b
Diego Cairone <diegocairone@gmail.com>
2022-07-03 16:24:25 -0300

    Initial commit

---
+++ src/test/java/com/cairone/im/AppTests.java
@@ -0,0 +1,13 @@
+package com.cairone.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
